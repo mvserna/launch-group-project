b7d1ffedd6cc2a9ff282a8293dbcd99b9b87d68f
Martin Villarreal Serna <martin.serna@gmail.com>
2021-11-10 16:10:30 -0500

    Implement feature tests for Devise authentication

---
+++ spec/features/01_user_signs_up_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+feature "user signs up" do
+  scenario "visting home page and clicking signup page" do
+    visit root_path
+    click_link "Sign Up"
+
+    expect(page).to have_content "New User Sign up"
+    fill_in "First name", with: "Suzy"
+    fill_in "Last name", with: "Q"
+    fill_in "Email", with: "newuser1@gmail.com"
+    fill_in "Password", with: "yayforcoffee"
+    fill_in "Password confirmation", with: "yayforcoffee"
+
+    click_button "Sign up"
+
+    expect(page).to have_content "Welcome! You have signed up successfully."
+  end
+end
