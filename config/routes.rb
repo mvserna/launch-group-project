6231670de60c9ca56615d32f7ba79c3d2daed101
Brian Lee <blee@Brians-MacBook-Air.local>
2021-11-18 14:47:50 -0500

    formatted line 15

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :coffeeshops, only: [:index, :show, :create] do
         resources :reviews, only: [:index]
-    end
+      end
       resources :votes, only: [:create]
     end
   end
