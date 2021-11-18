b3df40598de43aaefa6f6f50fb9b3f9091f591f3
Brian Lee <blee@Brians-MacBook-Air.local>
2021-11-18 12:57:02 -0500

    reset routes.rb to current main config. will update config files in future commit

--- config/routes.rb
+++ config/routes.rb
@@ -4,14 +4,16 @@ Rails.application.routes.draw do
   # root "homes#index"
 
   root "coffeeshops#index"
-  get "/coffeeshops/new", to: "coffeeshops#new"
+  
+  resources :coffeeshops, only: [:index, :show]
 
   namespace :api do
     namespace :v1 do
-      resources :coffeeshops, only: [:index, :show, :create, :new] do
+      resources :coffeeshops, only: [:index, :show] do
         resources :reviews, only: [:index]
+      end
+      resources :votes, only: [:create]
     end
-    resources :votes, only: [:create]
   end
 
 end
