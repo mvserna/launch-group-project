560dc77b10a75d59983e21d53374d65603b6d404
Martin Villarreal Serna <martin.serna@gmail.com>
2021-11-19 08:23:30 -0500

    Establish base styling for index and show pages

---
+++ app/javascript/react/components/CoffeeshopForm.js
@@ -0,0 +1,88 @@
+import React, { useState, useEffect } from "react"
+
+const CoffeeshopForm = (props) => {
+  const [coffeeFormData, setCoffeeFormData] = useState({
+    name: "",
+    address: "",
+    city: "",
+    state: "",
+    zip: "",
+    description: "",
+  })
+
+  const handleChange = (event) => {
+    event.preventDefault()
+
+    setCoffeeFormData({
+      ...coffeeFormData,
+      [event.currentTarget.name]: event.currentTarget.value,
+    })
+  }
+
+  const addShop = async () => {
+    try {
+      const response = await fetch("/api/v1/coffeeshops", {
+        credentials: "same-origin",
+        method: "POST",
+        body: JSON.stringify(coffeeFormData),
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+      })
+      if (!response.ok) {
+        throw new Error(`${response.status}: ${response.statusText}`)
+      }
+      const parseShopData = await response.json()
+      setCoffeeFormData({ ...coffeeFormData, parseShopData })
+    } catch (err) {
+      console.error(err)
+    }
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    addShop()
+    setCoffeeFormData({
+      name: "",
+      address: "",
+      city: "",
+      state: "",
+      zip: "",
+      description: "",
+    })
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        Name:
+        <input name="name" id="name" type="text" onChange={handleChange} />
+      </label>
+
+      <label>Address:</label>
+      <input name="address" id="address" type="text" onChange={handleChange} />
+
+      <label>City:</label>
+      <input name="city" id="city" type="text" onChange={handleChange} />
+
+      <label>State:</label>
+      <input name="state" id="state" type="text" onChange={handleChange} />
+
+      <label>Zip:</label>
+      <input name="zip" id="zip" type="text" onChange={handleChange} />
+
+      <label>Description:</label>
+      <input
+        name="description"
+        id="description"
+        type="text"
+        onChange={handleChange}
+      />
+
+      <input name="submit" type="submit" value="Add Shop" />
+    </form>
+  )
+}
+
+export default CoffeeshopForm
--- app/javascript/react/components/CoffeeshopTile.js
+++ app/javascript/react/components/CoffeeshopTile.js
@@ -2,17 +2,26 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 const CoffeeshopTile = (props) => {
-    return(
-        <div className="index tile">
-            <h1>
-                <Link to={`/coffeeshops/${props.id}`}>
-                    {props.name}
-                </Link>
-            </h1>
+  return(
+    <Link className="index tile link" to={`/coffeeshops/${props.id}`}>
+      <div className="index tile grid-container" >
+        <div className="grid-x grid-margin-x">
+          <div className="cell small-5">
+            <h4>
+                {props.name}
+            </h4>
+          </div>
+          <div className="cell small-6">
             <p>{ props.address }</p>
             <p>{ props.city }, {props.state} { props.zip }</p>  
+          </div>
+          <div className="cell small-1 arrow">
+            →
+          </div>
         </div>
-    )
+      </div>
+    </Link>
+  )
 }
 
 export default CoffeeshopTile
